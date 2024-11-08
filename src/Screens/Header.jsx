1d363bb011ca68a88d326e10b71a60e1137af039
Muhammad shahzad <shehzadmemon147@gmail.com>
2024-11-08 14:41:22 +0500

    Done

---
+++ src/Screens/Header.jsx
@@ -0,0 +1,22 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import IonIcon from 'react-native-vector-icons/Ionicons'
+import Entypo from 'react-native-vector-icons/Entypo' 
+
+const Header = () => {
+  return (
+    <View style={{flex: 1}}>
+    <View>
+      <Text style={{fontSize: 30, color:'#00ff99',}}>
+        WhatsApp
+      </Text>
+        </View>
+        <View>
+            <IonIcon name='camera-outline'/>
+            <Entypo name='dots-three-vertical'/>
+        </View>
+    </View>
+  )
+}
+
+export default Header
